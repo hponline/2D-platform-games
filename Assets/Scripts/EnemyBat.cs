093911637e99cb92b5718dbd5eb7dbd0ffda5556
Apo <abdurrahman_usta_41@hotmail.com>
2024-06-27 02:15:10 +0300

    version0.8
    EnemyBat,
    animation

---
+++ Assets/Scripts/EnemyBat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class EnemyBat : MonoBehaviour
+{
+    public GameObject player;
+    
+    private Rigidbody2D enemyRb;
+    
+    public float speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetTracking();
+    }
+
+    public void TargetTracking()
+    {
+        // Enemy objemiz takip etmesi için gameObjemizin konumundan
+        // kendi konumunu cikarıyor ve surekli olarak bizi takip ediyor.
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized * Time.deltaTime;
+        enemyRb.AddForce(lookDirection * speed);
+        
+
+    }
+
+    
+}
--- Assets/Scripts/HiddenPlatform.cs
+++ Assets/Scripts/HiddenPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjeManager : MonoBehaviour
+public class HiddenPlatform: MonoBehaviour
 {
     public GameObject sutun;
     
